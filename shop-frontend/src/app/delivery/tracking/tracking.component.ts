ed93c6cb69d3555a66dfc7565ef6260d6f655d3f
CC-Student <akshayraul07@gmail.com>
2025-05-09 12:15:12 +0200

    food-microservices

---
+++ shop-frontend/src/app/delivery/tracking/tracking.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {of, switchMap, timer} from "rxjs";
+import {AsyncPipe, JsonPipe, NgStyle} from "@angular/common";
+import {ActivatedRoute} from "@angular/router";
+import {DeliveryService} from "../services/delivery.service";
+
+@Component({
+  selector: 'app-tracking',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    JsonPipe,
+    NgStyle
+  ],
+  templateUrl: './tracking.component.html',
+  styleUrl: './tracking.component.scss'
+})
+export class TrackingComponent {
+
+  constructor(private route: ActivatedRoute, private deliveryService: DeliveryService) {}
+
+  delivery$ = timer(0, 3000).pipe(
+    switchMap(() => {
+      let orderId = this.route.snapshot.params['orderId'];
+      return this.deliveryService.get(orderId)
+    })
+  );
+
+
+
+}
