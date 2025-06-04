63144a44b8c305598c04467443b9092fe7066b5e
CC-Student <akshayraul07@gmail.com>
2025-06-04 05:40:12 +0200

    add service in go

---
+++ kitchen/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"io"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/kelseyhightower/envconfig"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/sethvargo/go-retry"
+)
+
+type Config struct {
+	RabbitMQHost string `default:"localhost"`
+}
+
+func main() {
+	
+}
