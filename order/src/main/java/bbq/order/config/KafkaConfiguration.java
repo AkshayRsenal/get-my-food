dc86f4d5d11701e4d2579a3d894da03873274270
CC-Student <akshayraul07@gmail.com>
2025-06-25 08:21:29 +0200

    Kafka config

--- order/src/main/java/bbq/order/config/KafkaConfiguration.java
+++ order/src/main/java/bbq/order/config/KafkaConfiguration.java
@@ -1,15 +1,19 @@
 package bbq.order.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.stereotype.Component;
+
 @Component
 @Configuration
 public class KafkaConfiguration {
 
     @Bean
     public NewTopic orderTopic() {
-        return TopicBuilder.name("orders")
-                .partitions(1)
-                .replicas(1)
-                .build();
+        return TopicBuilder.name("orders").build();
+
     }
-    
+
 }
